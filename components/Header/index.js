b237d528046e69054101bfd45451b5203ed5d078
Naterra <natalinyweb@hotmail.com>
2021-03-04 21:36:28 -0500

    Header container fix

--- components/Header/index.js
+++ components/Header/index.js
@@ -2,7 +2,7 @@ import styles from './Header.module.scss';
 import Logo from '../Logo';
 
 export default function Header(props){
-    return(<div className={styles.header}>
+    return(<div className={`container ${styles.header}`}>
         <Logo/>
     </div>)
 }
