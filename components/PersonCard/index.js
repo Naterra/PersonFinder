b0bb7b3c97d447dd1b711327750478a1a9a94b7f
Naterra <natalinyweb@hotmail.com>
2021-03-04 22:23:34 -0500

    use Next Image

--- components/PersonCard/index.js
+++ components/PersonCard/index.js
@@ -1,12 +1,17 @@
+import Image from 'next/image';
 import styles from './PersonCard.module.scss';
 
-export default function PersonCard({data}){
-    return(<div className={styles.personCard}>
-        <img className={styles.avatar} src={data.avatar} />
+export default function PersonCard({ data }) {
+	return (
+		<div className={styles.personCard}>
+			<div className={styles.avatar}>
+				<Image width={96} height={96} alt={data.name} layout="fixed" src={data.avatar} />
+			</div>
 
-        <div className={styles.wrapper}>
-            <div  className={styles.name}>{data.name}</div>
-            <div  className={styles.description}>{data.description}</div>
-        </div>
-    </div>);
+			<div className={styles.wrapper}>
+				<div className={styles.name}>{data.name}</div>
+				<div className={styles.description}>{data.description}</div>
+			</div>
+		</div>
+	);
 }
