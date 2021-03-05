09161052d5f68d88f4bdb47f863498fae5949a82
Naterra <natalinyweb@hotmail.com>
2021-03-05 02:20:00 -0500

    meta + opengraph

--- components/Layout.js
+++ components/Layout.js
@@ -4,10 +4,28 @@ import Header from '../components/Header';
 
 export default function Layout(props) {
 	const { meta = {} } = props;
+	const title = meta.title || 'Person Finder';
+	const description = meta.description || 'The Best Person Search Tool';
+
 	return (
 		<Fragment>
 			<Head>
-				<title>{meta.title || 'Person Finder'}</title>
+				<title>{title}</title>
+				<link rel="canonical" href={'/'} />
+				
+				<meta charSet="UTF-8" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+				<meta name="title" content={title} />
+				<meta name="description" content={description} />
+				<meta name="keywords" content={'search, person search'} />
+
+				{/*OG*/}
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content={title} />
+				<meta property="og:description" content={description} />
+				<meta property="og:site_name" content={'PersonFinder'} />
+				<meta property="og:image" content={'/logo.svg'} />
+				<meta property="og:url" content={'/'} />
 			</Head>
 			<main className="container">
 				<Header />
