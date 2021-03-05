5a82aa04cc8d1cf276053744d7afaed870076bcf
Naterra <natalinyweb@hotmail.com>
2021-03-05 01:51:24 -0500

    Add SearchBox debouncing

--- components/SearchBox/index.js
+++ components/SearchBox/index.js
@@ -1,19 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import styles from './SearchBox.module.scss';
 import { setSearchKeyword } from '../../store/actions';
 
 export default function SearchBox(props) {
 	const dispatch = useDispatch();
+	const [keyword, setKeyword] = useState();
 
-	const onInputChanged = e => {
-		const keyword = e.target.value;
-		dispatch(setSearchKeyword(keyword));
-	};
+	useEffect(() => {
+			let timer;
+			if (keyword) {
+				timer = setTimeout(() => {
+					dispatch(setSearchKeyword(keyword));
+				}, 1000);
+			}
+			return () => clearTimeout(timer);
+		},
+		[keyword]);
 
 	return (
 		<div className={styles.searchBox}>
 			<div className={styles.inputWrapper}>
-				<input onChange={onInputChanged} type="text" placeholder="Type a name" />
+				<input onChange={e => setKeyword(e.target.value)} type="text" placeholder="Type a name" />
 			</div>
 		</div>
 	);
