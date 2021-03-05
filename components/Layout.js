707913c888cbb4c095baedf4a47f29760b815063
Naterra <natalinyweb@hotmail.com>
2021-03-04 22:27:46 -0500

    Layout fix

--- components/Layout.js
+++ components/Layout.js
@@ -3,17 +3,15 @@ import Head from 'next/head';
 import Header from '../components/Header';
 
 export default function Layout(props) {
-	const { meta={}  } =props;
+	const { meta = {} } = props;
 	return (
 		<Fragment>
 			<Head>
 				<title>{meta.title || 'Person Finder'}</title>
 			</Head>
-			<main >
-				<Header/>
-				<div className='container'>
-					{props.children}
-				</div>
+			<main className="container">
+				<Header />
+				<div className="container content-container">{props.children}</div>
 			</main>
 		</Fragment>
 	);
